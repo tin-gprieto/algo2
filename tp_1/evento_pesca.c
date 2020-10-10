5fb8e09e30132a8a47b6dfc96210a7ef39679cf7
tin-gprieto <martin.gonzalez.prieto@gmail.com>
2020-10-09 22:15:38 -0300

    .gitignore agregado (test y ejecutable)

---
+++ tp_1/evento_pesca.c
@@ -0,0 +1,27 @@
+#include "evento_pesca.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+arrecife_t* crear_arrecife(const char* ruta_archivo){
+return NULL;
+}
+acuario_t* crear_acuario(){
+return NULL;
+}
+int trasladar_pokemon(arrecife_t* arrecife, acuario_t* acuario, bool (*seleccionar_pokemon) (pokemon_t*), int cant_seleccion){
+return 0;
+}
+void censar_arrecife(arrecife_t* arrecife, void (*mostrar_pokemon)(pokemon_t*)){
+
+}
+int guardar_datos_acuario(acuario_t* acuario, const char* nombre_archivo){
+return 0;
+}
+void liberar_acuario(acuario_t* acuario){
+
+}
+void liberar_arrecife(arrecife_t* arrecife){
+
+}
---
+++ tp_1/pesca.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main (int argc, char* argv[]){
+  
+return 0;
+}
