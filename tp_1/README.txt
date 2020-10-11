e93fcaf5f5b01517a88f6f9d27583cb0f0420e65
tin-gprieto <martin.gonzalez.prieto@gmail.com>
2020-10-11 01:44:29 -0300

    Comienzo de la estructura del MAIN -evento_pesca.c sin empezar

---
+++ tp_1/README.txt
@@ -0,0 +1,7 @@
+• Una breve introducción sobre el funcionamiento del trabajo presentado.
+• Una explicación de como compilarlo (línea de compilación) y como ejecutarlo (línea de ejecución).
+• Explique brevemente y de forma concisa los siguientes conceptos:
+	1.Punteros.
+	2.Aritmética de punteros.
+	3.Punteros a funciones.
+	4.Malloc y Realloc.
--- tp_1/pesca.c
+++ tp_1/pesca.c
@@ -2,8 +2,37 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include "evento_pesca.h"
+
+#define MAX_RUTA 100
+#define RESET "/e[0m"
+#define ROJO "/e[41m"
+#define VERDE "/e[42m"
+#define AMARILLO "/e[43m"
+
 
 int main (int argc, char* argv[]){
-  
+
+  if(argc<1){
+    printf( ROJO "No ingresaste la dirección del archivo!" RESET "\n");
+    return -1;
+  }
+
+  char ruta_arrecife[MAX_RUTA];
+  strcpy(ruta_arrecife, arg[1]);
+
+  arrecife_t* arrecife = crear_arrecife(ruta_arrecife);
+  if(arrecife == NULL){
+    printf( ROJO "El programa falló al crear la simulación del" AMARILLO "ararrecife" RESET "\n");
+    return -1;
+  }
+
+  acuario _t* acuario = crear_acuario();
+  if(acuario == NULL){
+    liberar_arrecife(arrecife);
+    printf( ROJO "El programa falló al crear la simulación del" AMARILLO "acuario" RESET "\n");
+    return -1;    }
+  }
+
 return 0;
 }
