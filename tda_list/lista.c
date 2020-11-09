be23723f39e5d54b116c7d97d5e6e4699249e1e0
tin-gprieto <martin.gonzalez.prieto@gmail.com>
2020-11-09 01:40:52 -0300

    v1: Funciones de lista.c creadas en su totalidad -Errores con los iteradores (mini_pruebas) -Faltan pruebas de pila, cola e iteradores

--- tda_list/lista.c
+++ tda_list/lista.c
@@ -4,91 +4,294 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+#define ERROR -1
+
+/*
+*Dado un nuevo nodo los inserta al final de la lista 
+inicializandolo con un elemento
+*/
+void insertar_al_final(lista_t *lista, nodo_t* nuevo_nodo){
+    if (lista->cantidad == 0) {
+        lista->nodo_inicio = nuevo_nodo;
+    }else{
+        (lista->nodo_fin)->siguiente = nuevo_nodo;
+    }
+    lista->nodo_fin = nuevo_nodo;
+}
+
+/*
+*Devuelve el puntero actual si es anterior al posterior
+sino avanza de forma recursiva
+*/
+nodo_t* nodo_buscar_anterior(nodo_t* actual, nodo_t* posterior){
+    if(actual->siguiente == posterior)
+        return actual;
+    return nodo_buscar_anterior(actual->siguiente, posterior);
+}
+
+/*
+*Borra el ultimo elemento de la lista
+*/
+void borrar_ultimo_elemento(lista_t* lista){
+    nodo_t *antecesor = nodo_buscar_anterior(lista->nodo_inicio,
+                                             lista->nodo_fin);
+    free(antecesor->siguiente);
+    antecesor->siguiente = NULL;
+    lista->nodo_fin = antecesor;
+}
+/*
+*Borra el primer elemento de la lista
+*/
+void borrar_primer_elemento(lista_t* lista){
+    nodo_t *aux;
+    aux = (lista->nodo_inicio)->siguiente;
+    free(lista->nodo_inicio);
+    lista->nodo_inicio = aux;
+}
+/*
+*Devuelve el puntero al nodo que se encuentre en la posicion 
+*pasada por parametro
+*/
+nodo_t* nodo_en_posicion(nodo_t* nodo, size_t contador, size_t posicion){
+    if(contador == posicion)
+        return nodo;
+    return nodo_en_posicion(nodo->siguiente, contador + 1, posicion);
+}
+
+/*
+*Destruye todos los nodos
+*/
+void nodo_destruir(nodo_t* nodo){
+    if(!nodo) 
+        return;
+    nodo_destruir(nodo->siguiente);
+    free(nodo);
+}
 
 lista_t* lista_crear(){
-    return NULL;
+    lista_t* lista = malloc(sizeof(lista_t));
+    if(!lista)
+        return NULL;
+    lista->nodo_inicio=NULL;
+    lista->nodo_fin=NULL;
+    lista->cantidad=0;
+    return lista;
 }
 
 int lista_insertar(lista_t* lista, void* elemento){
+    if(!lista) 
+        return ERROR;
+
+    nodo_t *nuevo_nodo = malloc(sizeof(nodo_t));
+    if (!nuevo_nodo)
+        return ERROR;
+    nuevo_nodo->elemento = elemento;
+    nuevo_nodo->siguiente = NULL;
+    insertar_al_final(lista, nuevo_nodo);
+    lista->cantidad++;
     return 0;
 }
 
 int lista_insertar_en_posicion(lista_t* lista, void* elemento, size_t posicion){
+    if(!lista)
+        return ERROR;
+
+    nodo_t *nuevo_nodo = malloc(sizeof(nodo_t));
+    if (!nuevo_nodo)
+        return ERROR;
+    nuevo_nodo->elemento = elemento;
+    if (lista->cantidad <= posicion){
+        nuevo_nodo->siguiente = NULL;
+        insertar_al_final(lista, nuevo_nodo);
+    }else if (posicion == 0){
+        nuevo_nodo->siguiente = lista->nodo_inicio;
+        lista->nodo_inicio = nuevo_nodo;
+    }else{
+        nodo_t * reemplazado = nodo_en_posicion(lista->nodo_inicio, 0, posicion);
+        nodo_t* antecesor = nodo_buscar_anterior(lista->nodo_inicio, reemplazado);
+        antecesor->siguiente = nuevo_nodo;
+        nuevo_nodo->siguiente = reemplazado;
+    }
+    lista->cantidad++;
     return 0;
 }
 
 int lista_borrar(lista_t* lista){
+    if((!lista) || (lista->cantidad == 0)) 
+        return ERROR;
+    if(lista->cantidad == 1){
+        free(lista->nodo_inicio);
+        lista->nodo_inicio = NULL;
+        lista->nodo_fin = NULL;
+    }else{
+        borrar_ultimo_elemento(lista);
+    }
+    lista->cantidad --;
     return 0;
 }
 
+
 int lista_borrar_de_posicion(lista_t* lista, size_t posicion){
+    if(!lista)
+        return ERROR;
+    if(lista->cantidad <= posicion){
+        borrar_ultimo_elemento(lista);
+    }else if (posicion == 0){
+        borrar_primer_elemento(lista);
+    }else{
+        nodo_t * borrado = nodo_en_posicion(lista->nodo_inicio, 0, posicion);
+        nodo_t * antecesor = nodo_buscar_anterior(lista->nodo_inicio, borrado);
+        antecesor->siguiente = borrado->siguiente;
+        free(borrado);
+    }
+    lista->cantidad --;
     return 0;
 }
 
 void* lista_elemento_en_posicion(lista_t* lista, size_t posicion){
-    return NULL;
+    if((!lista) || lista->cantidad <= posicion) 
+        return NULL;
+    nodo_t* nodo_buscado = nodo_en_posicion(lista->nodo_inicio, 0, posicion);     
+    return nodo_buscado->elemento;
 }
 
 void* lista_ultimo(lista_t* lista){
-    return  NULL;
+    if (!lista)
+        return NULL;
+    return (lista->nodo_fin)->elemento;
 }
 
 bool lista_vacia(lista_t* lista){
-    return false;
+    if ((!lista) || lista->cantidad != 0)
+        return false;
+    return true;
 }
 
 size_t lista_elementos(lista_t* lista){
-    return 0;
+    if (!lista)
+        return 0;
+    return lista->cantidad;
 }
 
 int lista_apilar(lista_t* lista, void* elemento){
+    if (!lista)
+        return ERROR;
+    nodo_t *nuevo_nodo = malloc(sizeof(nodo_t));
+    if (!nuevo_nodo)
+        return ERROR;
+    nuevo_nodo->elemento = elemento;
+    nuevo_nodo->siguiente = NULL;
+    insertar_al_final(lista, nuevo_nodo);
+    lista->cantidad++;
     return 0;
 }
 
 int lista_desapilar(lista_t* lista){
+    if ((!lista) || (lista->cantidad == 0))
+        return ERROR;
+    if (lista->cantidad == 1){
+        free(lista->nodo_inicio);
+        lista->nodo_inicio = NULL;
+        lista->nodo_fin = NULL;
+    }else{
+        borrar_ultimo_elemento(lista);
+    }
+    lista->cantidad--;
     return 0;
 }
 
 void* lista_tope(lista_t* lista){
-    return NULL;
+    if (!lista)
+        return NULL;
+    return (lista->nodo_fin)->elemento;
 }
 
 int lista_encolar(lista_t* lista, void* elemento){
+    if (!lista)
+        return ERROR;
+    nodo_t *nuevo_nodo = malloc(sizeof(nodo_t));
+    if (!nuevo_nodo)
+        return ERROR;
+    nuevo_nodo->elemento = elemento;
+    nuevo_nodo->siguiente = NULL;
+    insertar_al_final(lista, nuevo_nodo);
+    lista->cantidad++;
     return 0;
 }
 
 int lista_desencolar(lista_t* lista){
+    if ((!lista) || (lista->cantidad == 0))
+        return ERROR;
+    if (lista->cantidad == 1){
+        free(lista->nodo_inicio);
+        lista->nodo_inicio = NULL;
+        lista->nodo_fin = NULL;
+    }else{
+        borrar_primer_elemento(lista);
+    }
+    lista->cantidad--;
     return 0;
 }
 
 void* lista_primero(lista_t* lista){
-    return NULL;
+    if(!lista) 
+        return NULL;
+    return (lista->nodo_inicio)->elemento;
 }
 
 void lista_destruir(lista_t* lista){
-
+    if (!lista)
+        return;
+    if(lista->cantidad != 0)
+        nodo_destruir(lista->nodo_inicio);
+    free(lista);
+    lista = NULL;
 }
 
 lista_iterador_t* lista_iterador_crear(lista_t* lista){
-    return NULL;
+    if(!lista) 
+        return NULL;
+    lista_iterador_t* iterador = malloc(sizeof(lista_iterador_t));
+    if(!iterador)
+        return NULL;
+    iterador->corriente = lista->nodo_inicio;
+    iterador->lista = lista;
+    return iterador;
 }
 
 bool lista_iterador_tiene_siguiente(lista_iterador_t* iterador){
-    return false;
+    if(!((iterador->corriente)->siguiente))
+        return false;
+    return true;
 }
 
 bool lista_iterador_avanzar(lista_iterador_t* iterador){
-    return false;   
+    if (!((iterador->corriente)->siguiente))
+        return false;
+    iterador->corriente = (iterador->corriente)->siguiente;
+    return true;
 }
 
 void* lista_iterador_elemento_actual(lista_iterador_t* iterador){
-    return NULL;
+    if(!(iterador->corriente))
+        return NULL;
+    return (iterador->corriente)->elemento;
 }
 
 void lista_iterador_destruir(lista_iterador_t* iterador){
+    free(iterador);
+}
 
+size_t mover_por_nodos(nodo_t* nodo, bool (*funcion)(void *, void *), void *contexto){
+    if(!(nodo->siguiente))
+        return 0;
+    if(!funcion(nodo, contexto))
+        return 0;
+    return 1 + mover_por_nodos(nodo->siguiente, funcion, contexto);
 }
 
 size_t lista_con_cada_elemento(lista_t* lista, bool (*funcion)(void*, void*), void *contexto){
-    return 0;
+    if(!lista)
+        return 0;
+    return mover_por_nodos(lista->nodo_inicio, funcion, contexto);
 }
--- tda_list/lista_pruebas.c
+++ tda_list/lista_pruebas.c
@@ -2,10 +2,135 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include "pa2m.h"
+#include "pa2mm.h"
 #include "lista.h"
 
-int main(){
+#define ERROR -1
+void imprimir_separador(){
+    printf("------------------------------------------------------\n");
+}
+void probar_operaciones_comunes(){
+    pa2m_nuevo_grupo("OPERACIONES DE CREACIÓN");
+    lista_t * lista= lista_crear();
+    pa2m_afirmar(lista != NULL, "Se crea una lista");
+    pa2m_afirmar(lista_vacia(lista), "La lista está vacia");
+    pa2m_afirmar(lista_elementos(lista) == 0, "Tiene 0 elementos");
+    lista_destruir(lista);
+}
 
+bool se_inserto_bien(lista_t *lista, int elementos[], size_t cantidad){
+    bool son_iguales = true;
+    size_t i = (lista->cantidad) - cantidad;
+    int* lista_elemento;
+
+    while(son_iguales && (i<cantidad)){
+        lista_elemento = (int*) lista_elemento_en_posicion(lista, i);
+        if(lista_elemento != (elementos+i))
+            son_iguales = false;
+        i++;   
+    }
+    return son_iguales;
+}
+
+void probar_operaciones_lista(){
+    pa2m_nuevo_grupo("OPERACIONES EN LISTA");
+    lista_t *lista = lista_crear();
+    int primer_elemento=1;
+    pa2m_afirmar((lista_insertar(lista, &primer_elemento) == 0), "Se agrega el primer elemento");
+    pa2m_afirmar(lista_primero(lista) == &primer_elemento, "Es el primer elemento de la lista");
+    pa2m_afirmar(lista_ultimo(lista) == &primer_elemento, "Es el ultimo elemento de la lista")
+    pa2m_afirmar(lista_elementos(lista) == 1, "Tiene 1 elemento");
+    
+    int segundo_elemento = 2;
+    pa2m_afirmar((lista_insertar(lista, &segundo_elemento) == 0), "Se agrega otro elemento");
+    pa2m_afirmar(lista_ultimo(lista) == &segundo_elemento, "Es el ultimo elemento de la lista")
+    pa2m_afirmar(lista_elementos(lista) == 2, "Tiene 2 elementos");
+
+    pa2m_afirmar((lista_elemento_en_posicion(lista, 0) == &primer_elemento), "Elemento en posicio 0 es el primero")
+    pa2m_afirmar((lista_elemento_en_posicion(lista, 1) == &segundo_elemento), "Elemento en posicio 1 es el segundo")
+
+    imprimir_separador();
+
+    pa2m_afirmar((lista_borrar(lista) == 0), "Se borra el último elemento");
+    pa2m_afirmar(lista_elementos(lista) == 1, "Tiene 1 elemento");
+    pa2m_afirmar(lista_primero(lista)==lista_ultimo(lista), "El último elemento también es el primero");
+
+    pa2m_afirmar((lista_borrar(lista) == 0), "Se borra el único elemento");
+    pa2m_afirmar(lista_vacia(lista), "La lista está vacia");
+    pa2m_afirmar((lista_borrar(lista) == ERROR), "No se puede borrar algo de una lista vacia");
+
+    pa2m_nuevo_grupo("OPERACIONES AVANZADAS EN LISTA");
+
+    size_t cant_insertar = 6;
+    int elementos[] = {1,2,3,4,5,6};
+    size_t i = 0;
+    int estado = 0;
+    while ((i < cant_insertar) && (estado == 0)){
+        estado = lista_insertar(lista, elementos+i);
+        i++;
+    }
+
+    pa2m_afirmar(estado == 0, "Se insertan varios elementos");
+    pa2m_afirmar((lista_elementos(lista) == cant_insertar), "Se insertó la cantidad adecuada");
+    bool insercion = se_inserto_bien(lista, elementos, cant_insertar);
+    pa2m_afirmar((int) insercion, "Se insertaron en orden correcto");
+
+    imprimir_separador();
+
+    float elemento_extra = 3.5;
+    pa2m_afirmar((lista_insertar_en_posicion(lista, &elemento_extra, 4) == 0), "Se inserta un elemento en la posicion 4");
+    pa2m_afirmar((lista_elementos(lista) == (cant_insertar+1)), "Tiene la cantidad adecuada");
+    pa2m_afirmar((lista_borrar_de_posicion(lista, 4) == 0), "Se borra el elemento de la posicion 4");
+    pa2m_afirmar((lista_elementos(lista) == cant_insertar), "Tiene la cantidad adecuada");
+    pa2m_afirmar((lista_elemento_en_posicion(lista, 4) != &elemento_extra), "El elemento borrado no está más");
+    insercion = se_inserto_bien(lista, elementos, cant_insertar);
+    pa2m_afirmar((int)insercion, "La lista volvió al orden anterior de la inserción");
+
+    imprimir_separador();
+
+    pa2m_afirmar((lista_insertar_en_posicion(lista, &elemento_extra, 0) == 0), "Se inserta un elemento en la posicion 0");
+    pa2m_afirmar((lista_elementos(lista) == (cant_insertar + 1)), "Tiene la cantidad adecuada");
+    pa2m_afirmar((lista_borrar_de_posicion(lista, 0) == 0), "Se borra el elemento de la posicion 0");
+    pa2m_afirmar((lista_elementos(lista) == cant_insertar), "Tiene la cantidad adecuada");
+    pa2m_afirmar((lista_elemento_en_posicion(lista, 0) != &elemento_extra), "El elemento borrado no está más");
+    insercion = se_inserto_bien(lista, elementos, cant_insertar);
+    pa2m_afirmar((int)insercion, "La lista volvió al orden anterior de la inserción");
+
+    pa2m_nuevo_grupo("OPERACIONES LIMITES EN LISTA");
+
+    pa2m_afirmar((lista_insertar_en_posicion(lista, &elemento_extra, 500) == 0), "Se inserta un elemento en la posicion 500");
+    pa2m_afirmar((lista_elementos(lista) == (cant_insertar + 1)), "Tiene la cantidad adecuada");
+    pa2m_afirmar((lista_ultimo(lista) == &elemento_extra), "El elemento se encuentra al final");
+    pa2m_afirmar((lista_elemento_en_posicion(lista, 500) == NULL), "No existe la posicion 500");
+    lista_borrar(lista);
+    pa2m_afirmar((lista_ultimo(lista) != &elemento_extra), "Se borra el nuevo elemento y no está en el final")
+    pa2m_afirmar((lista_elementos(lista) == cant_insertar), "Tiene la cantidad adecuada");
+
+    //insertar 1000 elementos
+    
+    lista_destruir(lista);
+}
+
+void probar_operaciones_cola(){
+    pa2m_nuevo_grupo("OPERACIONES EN COLA");
+}
+
+void probar_operaciones_pila(){     
+    pa2m_nuevo_grupo("OPERACIONES EN PILA");
+}
+
+void probar_operaciones_iterador(){
+    pa2m_nuevo_grupo("OPERACIONES CON ITERADOR EXTERNO");
+
+    pa2m_nuevo_grupo("OPERACIONES CON ITERADOR INTERNO");
+}
+
+int main(){
+    probar_operaciones_comunes();
+    probar_operaciones_lista();
+    //probar_operaciones_cola();
+    //probar_operaciones_pila();
+    //probar_operaciones_iterador();
+    pa2m_mostrar_reporte();
     return 0;
 }
