ea4fd119bd2ec9189e86dc39a857831418f3fab1
tin-gprieto <martin.gonzalez.prieto@gmail.com>
2020-12-01 03:16:53 -0300

    v2: Falta borrar y recorrer abb_pruebas.c -Destruir sin problemas -Pruebas de buscar -Pruebas de insertar incompletas -Pruebas fallidas(TDD) de borrar y recorrer(incompleto) -Faltan pruebas de iterador interno abb.c -Implementación incompleta de recorridos e iterador -Falta implementación del borrado

--- tda_abb/abb.c
+++ tda_abb/abb.c
@@ -10,7 +10,14 @@
 #define IGUAL 0
 #define ERROR_PUNTERO NULL
 
+typedef struct array{
+    void** vector;
+    size_t posicion;
+    size_t tamanio;
+} array_t;
+
 bool es_hoja(nodo_abb_t* nodo){
+    if(!nodo) return true; 
     if ((!nodo->izquierda) && (!nodo->derecha))
         return true;
     return false;
@@ -24,42 +31,30 @@ void destruir_nodos_abb(nodo_abb_t* nodo){
             destruir_nodos_abb(nodo->izquierda);
         if(nodo->derecha)
             destruir_nodos_abb(nodo->derecha);
-    }
-    free(nodo);
+     }
+    if(nodo)
+        free(nodo);
 }
 
 /*
 *Inserta un nuevo nodo según corresponda con el comparador
 *Devuelve 0 si lo inserta correctamente o ERROR en caso contrario
 */
-int insertar_nodo_abb(abb_t* arbol, nodo_abb_t* nuevo_nodo, nodo_abb_t* corriente){
+int insertar_nodo_abb(abb_comparador comparador, nodo_abb_t* nuevo_nodo, nodo_abb_t* corriente){
     if(!corriente)
         return ERROR;
-    int orden = arbol->comparador(nuevo_nodo->elemento, corriente->elemento);
-    if(orden == MAYOR || orden == IGUAL){
+    int comparacion = comparador(nuevo_nodo->elemento, corriente->elemento);
+    if(comparacion == MAYOR || comparacion == IGUAL){
         if(!(corriente->derecha))
             corriente->derecha = nuevo_nodo;
         else
-            insertar_nodo_abb(arbol, nuevo_nodo, corriente->derecha);
-    }
-    else if (orden == MENOR){
+            insertar_nodo_abb(comparador, nuevo_nodo, corriente->derecha);
+    }else if (comparacion == MENOR){
         if (!(corriente->izquierda))
             corriente->izquierda = nuevo_nodo;
         else
-            insertar_nodo_abb(arbol, nuevo_nodo, corriente->izquierda);
+            insertar_nodo_abb(comparador, nuevo_nodo, corriente->izquierda);
     } 
-    return 0;
-
-}
-/*
-*Inserta un nuevo nodo como raiz dentro del árbol
-*Devuelve 0 si lo inserta correctamente o ERROR en caso contrario
-*/
-int insertar_nodo_raiz(abb_t* arbol, nodo_abb_t* nodo){
-    if((!arbol) || (!nodo)) return ERROR;
-    arbol->nodo_raiz = nodo;
-    nodo->izquierda = NULL;
-    nodo->derecha = NULL;
     return 0;
 }
 
@@ -74,17 +69,27 @@ abb_t *arbol_crear(abb_comparador comparador, abb_liberar_elemento destructor){
     arbol->destructor = destructor;
     return arbol;
 }
+/*
+*Se inicializa un nodo como hoja junto con su elemento
+*/
+void nodo_abb_inicializar(nodo_abb_t* nodo, void* elemento){
+    nodo->elemento = elemento; 
+    nodo->izquierda = NULL;
+    nodo->derecha = NULL;
+}
 
-int arbol_insertar(abb_t *arbol, void *elemento){
+int arbol_insertar(abb_t* arbol, void* elemento){
     if(!arbol || !elemento)
         return ERROR;
     nodo_abb_t* nuevo_nodo = malloc(sizeof(nodo_abb_t));
     if(!nuevo_nodo)
         return ERROR;
-    nuevo_nodo->elemento = elemento;
-    if(!arbol->nodo_raiz)
-        return  insertar_nodo_raiz(arbol, nuevo_nodo);
-    return insertar_nodo_abb(arbol, nuevo_nodo, arbol->nodo_raiz);
+    nodo_abb_inicializar(nuevo_nodo, elemento);
+    if(!arbol->nodo_raiz){
+        arbol->nodo_raiz = nuevo_nodo;
+        return 0;
+    }
+    return insertar_nodo_abb(arbol->comparador, nuevo_nodo, arbol->nodo_raiz);
 }
 int arbol_borrar(abb_t *arbol, void *elemento){
     return ERROR;
@@ -92,16 +97,16 @@ int arbol_borrar(abb_t *arbol, void *elemento){
 void *elemento_buscar(abb_comparador comparador, nodo_abb_t *corriente, void *elemento){
     if(!corriente)
         return NULL;
-    int orden = comparador(elemento, corriente->elemento);
-    if (orden == MAYOR)
+    int comparacion = comparador(elemento, corriente->elemento);
+    if (comparacion == MAYOR)
         return elemento_buscar(comparador, corriente->derecha, elemento);
-    else if (orden == MENOR)
+    else if (comparacion == MENOR)
         return elemento_buscar(comparador, corriente->izquierda, elemento);
     return corriente->elemento;
 }
 
 void *arbol_buscar(abb_t *arbol, void *elemento){
-    if(!arbol) 
+    if((!arbol) || (!elemento)) 
         return ERROR_PUNTERO;
     return elemento_buscar(arbol->comparador, arbol->nodo_raiz, elemento);
 }
@@ -115,15 +120,56 @@ bool arbol_vacio(abb_t *arbol){
     if (!arbol) return true;
     return !(arbol->nodo_raiz); 
 }
+
+
+bool funcion_cargar_array(void *elemento, void *array){
+    array_t* aux = (array_t*) array;
+    if(aux->posicion >= aux->tamanio)
+        return false;
+    aux->vector[aux->posicion] = elemento;
+    aux->posicion ++;
+    return true;
+}
+
+array_t inicializar_vector(void** array, size_t tamanio){
+    array_t aux;
+    aux.vector = array;
+    aux.posicion = 0;
+    aux.tamanio = tamanio;
+    return aux;
+}
+
+size_t recorrer_arbol(abb_t *arbol, void **array, size_t tamanio_array, size_t (*funcion_recorrido)(nodo_abb_t*, bool(*)(void*, void*), void*)){
+    if (!arbol || !arbol->nodo_raiz || tamanio_array <= 0)
+        return 0;
+    array_t vector = inicializar_vector(array, tamanio_array);
+    return funcion_recorrido(arbol->nodo_raiz, funcion_cargar_array, (void *)&vector);
+}
+
+size_t recorrer_inorden(nodo_abb_t* corriente, bool (*funcion)(void *, void *), void* extra){
+    return 0;
+}
+
+size_t recorrer_preorden(nodo_abb_t* corriente, bool (*funcion)(void *, void *), void* extra){
+    return 0;
+}
+
+size_t recorrer_postorden(nodo_abb_t* corriente, bool (*funcion)(void *, void *), void* extra){
+    return 0;
+}
+
 size_t arbol_recorrido_inorden(abb_t *arbol, void **array, size_t tamanio_array){
-    return 0;
+    return recorrer_arbol(arbol, array, tamanio_array, recorrer_inorden);
 }
+
 size_t arbol_recorrido_preorden(abb_t *arbol, void **array, size_t tamanio_array){
-    return 0;
+    return recorrer_arbol(arbol, array, tamanio_array, recorrer_preorden);
 }
+
 size_t arbol_recorrido_postorden(abb_t *arbol, void **array, size_t tamanio_array){
-    return 0;
+    return recorrer_arbol(arbol, array, tamanio_array, recorrer_postorden);
 }
+
 void arbol_destruir(abb_t *arbol){
     if(!arbol) return;
     if (arbol->nodo_raiz) 
@@ -132,5 +178,11 @@ void arbol_destruir(abb_t *arbol){
 
 }
 size_t abb_con_cada_elemento(abb_t *arbol, int recorrido, bool (*funcion)(void *, void *), void *extra){
-    return 0;
+    if(!arbol || !arbol->nodo_raiz)
+        return 0;
+    if(recorrido == ABB_RECORRER_INORDEN)
+        return recorrer_inorden(arbol->nodo_raiz, funcion, extra);
+    if (recorrido == ABB_RECORRER_PREORDEN)
+        return recorrer_preorden(arbol->nodo_raiz, funcion, extra);
+    return recorrer_postorden(arbol->nodo_raiz, funcion, extra);
 }
--- tda_abb/abb_pruebas.c
+++ tda_abb/abb_pruebas.c
@@ -7,6 +7,8 @@
 
 #define ERROR -1
 
+#define MAX_VECTOR 10
+
 int comparador_int(void* elemento_1, void* elemento_2){
     int primer_numero = *(int*) elemento_1;
     int segundo_numero = *(int*) elemento_2;
@@ -17,60 +19,225 @@ int comparador_int(void* elemento_1, void* elemento_2){
     return 0;
 }
 
-void destructor_heap(void* elemento){
+void destructor_dinamico(void* elemento){
     free(elemento);
 }
 
+int cargar_arbol(abb_t* arbol, int elementos[], int tope){
+    int i = 0;
+    int insercion = 0;
+    while ((insercion == 0) && (i < tope)){
+        insercion = arbol_insertar(arbol, (void *)&elementos[i]);
+        i++;
+    }
+    return insercion;
+}
+
+/*
+*Compara 
+*/
+bool comparar_vectores(int vector_1[], int recorrido[], int tamanio){
+    int i=0;
+    bool coincide=true;
+    while (coincide && i<tamanio){
+        if (vector_1[i] != recorrido[i])
+            coincide=false;
+        i++;
+    }
+    return coincide;
+}
+
 void probar_iterador(){
-
+    test_nuevo_grupo("Pruebas con iterador interno");
+    //completo
+    //en parte
+    //ninguno
 }
 
-void probar_postorden(){
+void probar_recorridos(){
+    abb_t* arbol = arbol_crear(comparador_int, NULL);
+    int elementos[] = {5, 3, 4, 7, 6, 9, 12};
+    int tope = 7;
+    cargar_arbol(arbol, elementos, tope);
+    
+    int* elementos_recorridos;
+    int** recorrido = &elementos_recorridos;
+    size_t tamanio_recorrido = 0;
+    size_t cant_recorrido = 0;
+    bool orden_adecuado = true;
 
+    test_nuevo_grupo("Recorrido Inorden");
+    int inorden[]={3, 4, 5, 6, 7, 9, 12};
+    tamanio_recorrido = 10;
+    cant_recorrido = arbol_recorrido_inorden(arbol, (void**) recorrido, tamanio_recorrido);
+    test_afirmar(cant_recorrido == tope, "Se recorrió la cantidad adecuada");
+    orden_adecuado = comparar_vectores(inorden, elementos_recorridos, tope);
+    test_afirmar(orden_adecuado, "Se recorrió en el orden correcto");
+
+    test_nuevo_grupo("Recorrido Preorden");
+    int preorden[] = {5, 3, 4, 7, 6, 9, 12};
+    tamanio_recorrido = 10;
+    cant_recorrido = arbol_recorrido_preorden(arbol, (void**) recorrido, tamanio_recorrido);
+    test_afirmar(cant_recorrido == tope, "Se recorrió la cantidad adecuada");
+    orden_adecuado = comparar_vectores(preorden, elementos_recorridos, tope);
+    test_afirmar(orden_adecuado, "Se recorrió en el orden correcto");
+
+    test_nuevo_grupo("Recorrido Inorden");
+    int postorden[] = {12, 9, 7, 6, 5, 4, 3};
+    tamanio_recorrido = 10;
+    cant_recorrido = arbol_recorrido_postorden(arbol, (void**) recorrido, tamanio_recorrido);
+    test_afirmar(cant_recorrido == tope, "Se recorrió la cantidad adecuada");
+    orden_adecuado = comparar_vectores(postorden, elementos_recorridos, tope);
+    test_afirmar(orden_adecuado, "Se recorrió en el orden correcto");
+
+    arbol_destruir(arbol);
 }
 
-void probar_preorden(){
-
-}
-
-void  probar_inorden(){
-
-}
-
-void probar_busqueda(){
-
-}
 
 void probar_borrado(){
+    abb_t *arbol = arbol_crear(comparador_int, NULL);
+    int elementos[] = {5, 3, 4, 7, 6, 9, 12};
+    int tope = 7;
+    cargar_arbol(arbol, elementos, tope);
 
+    int *elementos_encontrados;
+    int **recorrido = &elementos_encontrados;
+    size_t tamanio_recorrido = 0;
+    size_t cant_recorrido = 0;
+    tamanio_recorrido = 10;
+    bool orden_adecuado = false;
+
+    test_nuevo_grupo("Pruebas con Borrar");
+
+    test_nuevo_sub_grupo("Con Predecesor inorden");
+    test_afirmar(arbol_borrar(arbol, &elementos[0]) == 0, "Se elimina el elemento");
+    test_afirmar(arbol_buscar(arbol, &elementos[0]) == NULL, "El elemento no se encuentra en el árbol");
+    int borrado_1_esperado[] = {4, 3, 7, 6, 9, 12};
+    cant_recorrido = arbol_recorrido_preorden(arbol, (void **)recorrido, tamanio_recorrido);
+    test_afirmar(cant_recorrido == tope - 1, "El árbol se encuentra con la cantidad de elementos adecuada");
+    orden_adecuado = comparar_vectores(borrado_1_esperado, elementos_encontrados, tope - 1);
+    test_afirmar(orden_adecuado, "Se recorrió en el orden correcto");
+
+    test_nuevo_sub_grupo("No tiene sub-árbol menor");
+    test_afirmar(arbol_borrar(arbol, &elementos[5]) == 0, "Se elimina el elemento");
+    test_afirmar(arbol_buscar(arbol, &elementos[5]) == NULL, "El elemento no se encuentra en el árbol");
+    int borrado_2_esperado[] = {4, 3, 7, 6, 9, 12};
+    cant_recorrido = arbol_recorrido_preorden(arbol, (void **)recorrido, tamanio_recorrido);
+    test_afirmar(cant_recorrido == tope - 2, "El árbol se encuentra con la cantidad de elementos adecuada");
+    orden_adecuado = comparar_vectores(borrado_2_esperado, elementos_encontrados, tope - 2);
+    test_afirmar(orden_adecuado, "Se recorrió en el orden correcto");
+
+    test_nuevo_sub_grupo("El hijo menor es hoja");
+    test_afirmar(arbol_borrar(arbol, &elementos[3]) == 0, "Se elimina el elemento");
+    test_afirmar(arbol_buscar(arbol, &elementos[3]) == NULL, "El elemento no se encuentra en el árbol");
+    int borrado_3_esperado[] = {4, 3, 7, 6, 9, 12};
+    cant_recorrido = arbol_recorrido_preorden(arbol, (void **)recorrido, tamanio_recorrido);
+    test_afirmar(cant_recorrido == tope - 3, "El árbol se encuentra con la cantidad de elementos adecuada");
+    orden_adecuado = comparar_vectores(borrado_3_esperado, elementos_encontrados, tope - 3);
+    test_afirmar(orden_adecuado, "Se recorrió en el orden correcto");
+
+    test_nuevo_sub_grupo("No tiene hijos");
+    test_afirmar(arbol_borrar(arbol, &elementos[6]) == 0, "Se elimina el elemento");
+    test_afirmar(arbol_buscar(arbol, &elementos[6]) == NULL, "El elemento no se encuentra en el árbol");
+    int borrado_4_esperado[] = {4, 3, 7, 6, 9, 12};
+    cant_recorrido = arbol_recorrido_preorden(arbol, (void **)recorrido, tamanio_recorrido);
+    test_afirmar(cant_recorrido == tope - 4, "El árbol se encuentra con la cantidad de elementos adecuada");
+    orden_adecuado = comparar_vectores(borrado_4_esperado, elementos_encontrados, tope - 4);
+    test_afirmar(orden_adecuado, "Se recorrió en el orden correcto");
+
+    arbol_destruir(arbol);
+}
+
+
+void probar_busqueda(){
+    test_nuevo_grupo("Pruebas con Buscar");
+    abb_t *arbol = arbol_crear(comparador_int, NULL);
+
+    int numero_insertado = 5;
+    int *elemento_insertado = &numero_insertado;
+    arbol_insertar(arbol, (void *)elemento_insertado);
+    test_afirmar((int *)arbol_buscar(arbol, (void*) elemento_insertado) == elemento_insertado, "Se encuentra el único elemento del árbol");
+    test_afirmar(*(int*)arbol_buscar(arbol, (void*) elemento_insertado) == numero_insertado, "Coinciden los valores de ambos elementos");
+
+    int elementos[] = {5, 3, 4, 7, 6, 9, 12};
+    int tope = 7;
+    cargar_arbol(arbol, elementos, tope);
+
+    int j = 0;
+    bool se_encuentra = true;
+    while ((se_encuentra) && (j<tope)){
+        if(*(int *)arbol_buscar(arbol, (void *)&elementos[j]) != elementos[j])
+           se_encuentra = false;
+        j++;
+    }
+    
+    test_afirmar(se_encuentra, "Se encuentra todos los elemento en un árbol con varias inserciones");
+
+    int numero_fuera = 10;
+    int *elemento_fuera = &numero_fuera;
+    test_afirmar(arbol_buscar(arbol, (void*) elemento_fuera) == NULL, "No se encuentra un elemento no insertado");
+
+    test_afirmar(arbol_buscar(arbol, NULL) == NULL, "No se encuentra un elemento nulo");
+
+    arbol_destruir(arbol);
 }
 
 void probar_insercion(){
-    test_nuevo_sub_grupo("Inserciones básicas");
+    test_nuevo_grupo("Pruebas básicas con Insertar");
+
+    test_nuevo_sub_grupo("Inserción de raiz");
     abb_t* arbol = arbol_crear(comparador_int, NULL);
     int numero_1 = 5;
     int* elemento_1 = &numero_1;
     test_afirmar(arbol_insertar(arbol, (void*)elemento_1) != ERROR,"Se inserta un primer elemento correctamente");
     test_afirmar((int*) arbol_raiz(arbol) == elemento_1, "El primer elemento es la raíz");
+    test_afirmar(*(int*) arbol_raiz(arbol) == numero_1, "El elemento corresponde con el ingresado");
 
+    test_nuevo_sub_grupo("Inserción de un elemento mayor");
     int numero_2 = 3;
     int* elemento_2 = &numero_2;
     arbol_insertar(arbol, (void*) elemento_2);
     test_afirmar(arbol->nodo_raiz->izquierda->elemento == elemento_2, "Se encuentra a la izquierda de la raíz");
+    test_afirmar(*(int *)(arbol->nodo_raiz->izquierda->elemento) == numero_2, "El elemento corresponde con el ingresado");
     test_afirmar((int *)arbol_raiz(arbol) == elemento_1, "La raíz sigue siendo la misma");
 
+    test_nuevo_sub_grupo("Inserción de un elemento menor");
     int numero_3 = 7;
     int *elemento_3 = &numero_3;
     test_afirmar(arbol_insertar(arbol, (void *)elemento_3) != ERROR, "Se inserta un elemento mayor correctamente");
     test_afirmar(arbol->nodo_raiz->derecha->elemento == elemento_3, "Se encuentra a la derecha de la raíz");
+    test_afirmar(*(int *)(arbol->nodo_raiz->derecha->elemento) == numero_3, "El elemento corresponde con el ingresado");
+    test_afirmar((int *)arbol_raiz(arbol) == elemento_1, "La raíz sigue siendo la misma");
+    
+    test_nuevo_sub_grupo("Inserción de un elemento repetido");
+    int numero_4 = 5;
+    int *elemento_4 = &numero_4;
+    test_afirmar(arbol_insertar(arbol, (void *)elemento_4) != ERROR, "Se inserta un elemento igual a la raíz");
+    test_afirmar(arbol->nodo_raiz->derecha->izquierda->elemento == elemento_4, "Se encuentra a la izquierda del mayor");
+    test_afirmar(*(int *)(arbol->nodo_raiz->derecha->izquierda->elemento) == numero_4, "El elemento corresponde con el ingresado");
     test_afirmar((int *)arbol_raiz(arbol) == elemento_1, "La raíz sigue siendo la misma");
 
     arbol_destruir(arbol);
+
+    test_nuevo_sub_grupo("Insertar varios elementos");
+
+    abb_t* arbol_2 = arbol_crear(comparador_int, NULL);
+
+    int elementos[]={5,3,4,7,6,9,12};
+    int tope = 7;
+    int insercion = cargar_arbol(arbol_2, elementos, tope);
+
+    test_afirmar(insercion == 0, "Se insertaron correctamente varios elementos");
+    test_afirmar(arbol_insertar(arbol_2, NULL) == ERROR, "No se inserta un elemento nulo");
+
+    arbol_destruir(arbol_2);
 }
 
 void probar_basicas(){
+    test_nuevo_grupo("Pruebas básicas");
+
     test_nuevo_sub_grupo("Creación del árbol");
-    abb_t* arbol = arbol_crear(comparador_int, destructor_heap);
+    abb_t* arbol = arbol_crear(comparador_int, destructor_dinamico);
     test_afirmar(arbol != NULL, "Se crea un árbol");
     test_afirmar(arbol_vacio(arbol), "El árbol creado está vacio");
     test_afirmar(arbol_raiz(arbol) == NULL, "El árbol no tiene raíz");
@@ -80,7 +247,7 @@ void probar_basicas(){
     arbol = arbol_crear(comparador_int, NULL);
     test_afirmar(arbol != NULL, "Se crea un árbol sin destructor");
     arbol_destruir(arbol);
-    arbol = arbol_crear(NULL, destructor_heap);
+    arbol = arbol_crear(NULL, destructor_dinamico);
     test_afirmar(arbol == NULL, "No se crea un árbol sin comparador");
     test_afirmar(arbol_vacio(arbol), "Un árbol nulo está vacío");
     test_afirmar(arbol_raiz(arbol) == NULL, "Un árbol nulo no tiene raíz");
@@ -88,21 +255,12 @@ void probar_basicas(){
 }
 
 int main(){
-    test_nuevo_grupo("Pruebas básicas");
-    probar_basicas(); //Crear, Vacio, Destruir, Raiz
-    test_nuevo_grupo("Pruebas con Insertar");
+   
+    probar_basicas(); //Crear, Vacio, Destruir, Raiz   
     probar_insercion(); //Insertar, Raiz
-    test_nuevo_grupo("Pruebas con Borrar");
-    probar_borrado(); 
-    test_nuevo_grupo("Pruebas con Buscar");
     probar_busqueda(); 
-    test_nuevo_grupo("Recorrido Inorden");
-    probar_inorden();
-    test_nuevo_grupo("Recorrido Preorden");
-    probar_preorden();
-    test_nuevo_grupo("Recorrido Postorden");
-    probar_postorden();
-    test_nuevo_grupo("Pruebas con Iterador interno");
+    probar_borrado(); 
+    probar_recorridos();
     probar_iterador();
     test_mostrar_reporte();
     return 0;
