3be515e97921a4935ff21c75d3b57e9b11f6c530
tin-gprieto <martin.gonzalez.prieto@gmail.com>
2021-04-01 01:07:47 -0300

    PRUEBAS Completas

--- tda_hash/code/hash.h
+++ tda_hash/code/hash.h
@@ -7,7 +7,7 @@
 #include <stddef.h>
 #include <string.h>
 
-#include "toolbox/code/TDAs/lista/lista.h"
+#include "../toolbox/code/TDAs/lista/lista.h"
 
 /* 
  * Destructor de los datos almacenados en el hash.  Cada vez que un
@@ -17,17 +17,21 @@
 typedef void (*hash_destructor_t)(void*);
 
 typedef struct hash{
-    lista_t** tabla;
-    size_t tamanio;
-    size_t ocupado;
-    size_t elementos;
+    lista_t** tabla;  //vector de listas
+    size_t tamanio;   //tamanio de la tabla
+    size_t ocupado;   //espacios ocupados
+    size_t elementos; //cantidad de elementos
     hash_destructor_t destructor;
 }hash_t;
 
-typedef struct dato{
-    void* elemento;
-    const char* clave;
-}dato_t;
+/* Iterador externo para el HASH */
+typedef struct hash_iter {
+    hash_t* hash;
+    size_t pos_tabla;
+    size_t pos_lista_actual;
+    size_t contador;
+    const char* clave_actual;
+}hash_iterador_t;
 
 /*
  * Crea el hash reservando la memoria necesaria para el.
@@ -102,5 +106,34 @@ void hash_destruir(hash_t* hash);
  *
  */
 size_t hash_con_cada_clave(hash_t* hash, bool (*funcion)(hash_t* hash, const char* clave, void* aux), void* aux);
+
+/*
+ * ITERADOR EXTERNO
+ * Crea un iterador de claves para el hash reservando la memoria
+ * necesaria para el mismo. El iterador creado es válido desde su
+ * creación hasta que se modifique la tabla de hash (insertando o
+ * removiendo elementos).
+ *
+ * Devuelve el puntero al iterador creado o NULL en caso de error.
+ */
+hash_iterador_t* hash_iterador_crear(hash_t* hash);
+
+/*
+ * Devuelve la próxima clave almacenada en el hash y avanza el iterador.
+ * Devuelve la clave o NULL si no habia mas.
+ */
+const char* hash_iterador_siguiente(hash_iterador_t* iterador);
+
+/*
+ * Devuelve true si quedan claves por recorrer o false en caso
+ * contrario o de error.
+ */
+bool hash_iterador_tiene_siguiente(hash_iterador_t* iterador);
+
+/*
+ * Destruye el iterador del hash.
+ */
+void hash_iterador_destruir(hash_iterador_t* iterador);
+
 
 #endif /* __HASH_H__ */
